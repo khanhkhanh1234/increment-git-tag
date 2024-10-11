e23768a6b3a69a13c1a72bae7e28baf9ef04910e
QuocKhanh Gitlab <quockhanhlanglim@gmail.com>
2024-10-11 11:21:46 +0700

    initial commit fix 345

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const exec = require('@actions/exec');
 
 async function run() {
     try {
-        const version = core.getInput('version-type');
+        const versionType = core.getInput('version-type');
         const src = __dirname;
-        await exec.exec(`${src}/git_update.sh -v ${version}`);
+        await exec.exec(`${src}/git_update.sh -v ${versionType}`);
     }
     catch (error) {
         core.setFailed(error.message);
